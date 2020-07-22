415c20d382c103b47225e5e055e6caf213aec226
john leonhel ang <38998788+leonhelthe@users.noreply.github.com>
2020-07-22 21:08:17 +0800

    Add files via upload
    initial commit

---
+++ precache-manifest.48e5b0f157d9fc9fec98f7430b420801.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5850990eb9b6909e1cc943dfec9da191",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25611b0c9c385689d727",
+    "url": "/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "3a2a673bdcccfecdb228",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "25611b0c9c385689d727",
+    "url": "/static/js/2.0c1a4483.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0c1a4483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a2a673bdcccfecdb228",
+    "url": "/static/js/main.7f437dd3.chunk.js"
+  },
+  {
+    "revision": "a3d3d71e79b33f3da9ff",
+    "url": "/static/js/runtime-main.3d0dd2a1.js"
+  }
+]);
---
+++ products.js
@@ -0,0 +1,32 @@
+[
+  {
+    "image": "bq3381-800-a.png",
+    "name": "Air Jordan XXXIV 'Amber Rise'",
+    "brand": "Jordan"
+  },
+  {
+    "image": "bq3381-003-a.png",
+    "name": "Air Jordan XXXIV 'Black Cat'",
+    "brand": "Jordan"
+  },
+  {
+    "image": "bq3381-100-a.png",
+    "name": "Air Jordan XXXIV 'Bred'",
+    "brand": "Jordan"
+  },
+  {
+    "image": "bq3381-001-a.png",
+    "name": "Air Jordan XXXIV 'Eclipse'",
+    "brand": "Jordan"
+  },
+  {
+    "image": "bq3381-600-a.png",
+    "name": "Air Jordan XXXIV 'Infrared'",
+    "brand": "Jordan"
+  },
+  {
+    "image": "cu3475-100-a.png",
+    "name": "Air Jordan XXXIV Low PF 'Crispy White'",
+    "brand": "Jordan"
+  }
+]
